1f1963c7f18c957b9d130d7ef4a22c78239e1e7e
KANDUKURI SAIRAM <47742630+kksairam19061996@users.noreply.github.com>
2021-05-03 07:31:45 +0530

    Python Logging

---
+++ 18) Python Logging/student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import logging
+
+logging.basicConfig()
+logging.critical("This is critical message") # This will execute on console
+logging.error("This is error messagae") # This will execute on console
+logging.warning("This is warning message") # This will execute on console
+logging.info("This is info message")  
+logging.debug("This is debug message")
+
